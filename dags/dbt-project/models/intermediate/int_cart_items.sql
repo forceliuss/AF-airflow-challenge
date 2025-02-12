35c02fee782708501d66735b10fd51e99a7a712b
Alberto Forcelius <forcelius99@gmail.com>
2025-02-12 12:36:17 +0000

    fix: fix typo into models

--- dags/dbt-project/models/intermediate/int_cart_items.sql
+++ dags/dbt-project/models/intermediate/int_cart_items.sql
@@ -12,12 +12,9 @@ WITH CART_ITEMS AS (
         (ITEM->>'product_id')::INTEGER AS PRODUCT_ID,
         (ITEM->>'product_name')::TEXT AS PRODUCT_NAME,
         (ITEM->>'quantity')::INTEGER AS QUANTITY,
-        (ITEM->>'unit_price')::DECIMAL(10,
-        2) AS UNIT_PRICE,
-        (ITEM->>'line_total')::DECIMAL(10,
-        2) AS LINE_TOTAL,
-        (ITEM->>'discount')::DECIMAL(10,
-        2) AS DISCOUNT
+        (ITEM->>'unit_price')::DECIMAL(10,2) AS UNIT_PRICE,
+        (ITEM->>'line_total')::DECIMAL(10,2) AS LINE_TOTAL,
+        (ITEM->>'discount')::DECIMAL(10,2) AS DISCOUNT
     FROM
         CART_ITEMS
 )
